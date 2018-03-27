6ff9ba2cd2f276f334389c0090df9e559dd7a677
Seth Wax <sethlwax@gmail.com>
2018-03-27 20:30:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|a, b| b-a}
+end
+
+def sort_array_char_count(array)
+  array.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  placeholder = array[destination_index]
+  array[destination_index] = array[index]
+  array[index] = placeholder
+  array
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 1, 2)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |string|
+    string[2] = '$'
+    kesha_array << string
+  end
+  kesha_array
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect{|string, index| index == 1 ? string : string + 's'}
+end
